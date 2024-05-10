4c4f57ac9b20c170a57773e35e0c83b2fdf7de11
ItziaOCejo <itzia_02@hotmail.com>
2024-05-09 21:10:47 -0600

    Agrego los archivos con la tarjeta de información

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "./Hero.css";
+import Typography from '@mui/material/Typography';
+import Card from "../personalCard/card";
+
+function Hero () {
+    return (
+        <>
+        <Typography variant="h1" gutterBottom>Welcome</Typography>
+        <Card />
+        </>
+    );
+}
+
+export default Hero; 
